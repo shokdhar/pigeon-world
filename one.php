fefce595ca53f92a6b4f25b23c515153dfd6b57b
shokdhar <79388142+shokdhar@users.noreply.github.com>
2021-03-07 14:42:37 +0530

    Add files via upload

---
+++ one.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "i like programming"; ?>
+  </body>
+</html>
